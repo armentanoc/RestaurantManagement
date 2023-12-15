c938a97134e4c8dec203211985bc1ed7815a1b5e
paulaandrezza <paulaandrezza25@gmail.com>
2023-12-15 16:51:19 -0300

    init funcionarios

---
+++ RestaurantManagement.Core/Model/Pessoas/Gerente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Core.Model.Pessoas
+{
+    internal class Gerente : Funcionario
+    {
+        public Gerente(string Nome, decimal Salario) : base(Nome, Salario) { }
+    }
+}

602a96581a361c0362c080e870fbe991888d4be1
little-junior <rampagedolol@outlook.com>
2023-12-15 17:05:06 -0300

    Adicionar enum TipoPagamento

---
+++ RestaurantManagement.Core/Modelos/Enum/TipoPagamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Core.Modelos.Enum
+{
+    public enum TipoPagamento
+    {
+        CartaoDebito = 1,
+        CartaoCredito = 2,
+        Dinheiro = 3,
+        ValeAlimentacao = 4,
+        ValeRefeicao = 5
+    }
+}

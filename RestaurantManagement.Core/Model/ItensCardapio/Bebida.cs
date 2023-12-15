1dd5b50d14dc9a778d408900fbd856f83e8a8956
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-15 17:10:24 -0300

    feat: produto, prato e bebida

---
+++ RestaurantManagement.Core/Model/ItensCardapio/Bebida.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Core.Model.ItensCardapio
+{
+    internal class Bebida : Produto
+    {
+        public Bebida(string nome, double volume, bool alcoolico, string descricao, double preco) : base(nome, descricao, preco)
+        {
+            this.Volume = volume;
+            this.Alcoolico = alcoolico;
+        }
+
+        public double Volume { get; set; }
+        public bool Alcoolico { get; set; }
+
+
+        public override string ToString()
+        {
+            string ehalcoolico = Alcoolico ? ("Bebida alcoólica") : ("Bebida não alcoólica");
+            return $"ID {Id} | {Nome} | R${Preco}\n->{ehalcoolico} | {Volume} ml";
+        }
+    }
+}
---
+++ RestaurantManagement.Core/Model/ItensCardapio/Prato.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Core.Model.ItensCardapio
+{
+    internal class Prato : Produto
+    {
+        public bool Vegetariano { get; set; }
+
+        public Prato(string nome, bool vegetariano, string descricao, double preco) : base(nome, descricao, preco)
+        {
+            this.Vegetariano = vegetariano;
+        }
+        public override string ToString()
+        {
+            string ehvegetariano = Vegetariano ? ("Prato vegetariano") : ("Prato de origem animal");
+            return $"ID {Id} | {Nome} | R${Preco}\n->{ehvegetariano}";
+        }
+    }
+}

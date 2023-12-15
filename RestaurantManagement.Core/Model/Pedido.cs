f05b66af1312b6d5a89fc38f61ed6d672c3e8dc6
daisyr <daisyr@ME003504>
2023-12-15 16:55:24 -0300

    Adicionar classe pedido

---
+++ RestaurantManagement.Core/Model/Pedido.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Core.Model
+{
+    internal class Pedido
+    {
+        private List<Tuple<Item, int>> _itensPedidos;
+        private Mesa mesa;
+        private bool _foiEntregue;
+        private Pagamento pagamento;
+
+        public Pedido(Mesa mesa)
+        {
+            this.mesa = mesa;
+        }
+
+        public void AdicionarItens(Item item, int quantidade)
+        {
+            _itensPedidos.Add(new Tuple<Item, int>(item, quantidade));
+        }
+    }
+}

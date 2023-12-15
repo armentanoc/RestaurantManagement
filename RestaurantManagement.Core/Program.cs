29cea95833455d6105cb2cf631648334370c93c4
armentanoc <armentanocarolina@gmail.com>
2023-12-15 16:55:09 -0300

    Preliminar menu.

--- RestaurantManagement.Core/Program.cs
+++ RestaurantManagement.Core/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace RestaurantManagement.Core
+﻿using RestaurantManagement.ConsoleInteraction;
+
+namespace RestaurantManagement.Core
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] menuPrincipal = { "Um", "Dois", "Três", "Sair" };
+            Menu opcoes = new Menu(menuPrincipal);
+
+            while (true)
+            {
+                int selecaoUsuario = opcoes.ExibirMenu(Titulo.Principal());
+                AnalisarEscolhasUsuario(selecaoUsuario);
+            }
+        }
+        private static void AnalisarEscolhasUsuario(int selecaoUsuario)
+        {
+            switch (selecaoUsuario)
+            {
+                case 0:
+                    Console.WriteLine("Um");
+                    AguardarTresSegundos();
+                    break;
+                case 1:
+                    Console.WriteLine("Dois");
+                    AguardarTresSegundos();
+                    break;
+                case 2:
+                    Console.WriteLine("Três");
+                    AguardarTresSegundos();
+                    break;
+                case 3:
+                    Console.WriteLine("Programa encerrado");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    AguardarTresSegundos();
+                    break;
+            }
+        }
+
+        private static void AguardarTresSegundos()
+        {
+            Console.CursorVisible = false;
+            Thread.Sleep(3000);
         }
     }
 }

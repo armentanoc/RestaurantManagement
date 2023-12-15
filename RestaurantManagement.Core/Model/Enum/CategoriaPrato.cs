c51777a71377f738b9bbdb995f5087852c08e06d
vitorialira92 <vitoriabusinesscontact@gmail.com>
2023-12-15 17:30:02 -0300

    feat: enum para categoria de um prato

---
+++ RestaurantManagement.Core/Model/Enum/CategoriaPrato.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Core.Model.Enum
+{
+    internal enum CategoriaPrato
+    {
+        Entrada = 1,
+        Principal = 2,
+        Sobremesa = 3
+    }
+}

78f0e41278f75d8293dca9ee147a58a5592ee4bc
daisyr <daisyr@ME003504>
2023-12-15 17:39:48 -0300

    Adicionar o pagamento

---
+++ RestaurantManagement.Core/Modelos/Pagamento/Pagamento.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantManagement.Core.Modelos.Pedido;
+using RestaurantManagement.Core.Modelos.Enum;
+
+namespace RestaurantManagement.Core.Modelos.Pagamento
+{
+    internal class Pagamento
+    {
+        private Pedido _pedido;
+        public string _id { get; set; }
+        private bool _foiPago;
+        private decimal _valorTotal;
+        private TipoPagamento _tipoPagamento;
+
+        public Pagamento(Pedido pedido, TipoPagamento tipoPagamento)
+        {
+            _pedido = pedido;
+            _id = _pedido.id;
+            _tipoPagamento = tipoPagamento;
+        }
+
+
+    }
+}
---
+++ RestaurantManagement.Core/Modelos/Pedido/Pedido.cs
@@ -0,0 +1,43 @@
+﻿using RestaurantManagement.Core.Modelos.Mesas;
+using RestaurantManagement.Core.Modelos.Pagamento;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Core.Modelos.Pedido
+{
+    internal class Pedido
+    {
+        public string id { get; set; }
+        private List<Tuple<Item, int>> _itensPedidos;
+        private Mesa _mesa;
+        private bool _foiEntregue;
+        private Pagamento _pagamento;
+
+        public Pedido(Mesa mesa)
+        {
+            id = Guid.NewGuid().ToString();
+            _mesa = mesa;
+            _foiEntregue = false;
+        }
+
+        public void AdicionarItens(Item item, int quantidade)
+        {
+            _itensPedidos.Add(new Tuple<Item, int>(item, quantidade));
+        }
+
+        public void EntregarPedido()
+        {
+            _foiEntregue = true;
+        }
+
+        public void PagarPedido(Pagamento formaDePagamento)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}

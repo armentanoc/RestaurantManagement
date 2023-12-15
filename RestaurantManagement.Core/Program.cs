dbfc20f4338cbac7554fbea91c63253da1d4c927
armentanoc <armentanocarolina@gmail.com>
2023-12-15 16:12:46 -0300

    Add project files.

---
+++ RestaurantManagement.Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantManagement.Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

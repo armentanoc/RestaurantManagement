d94b2223b709f355d09f653bec619c556a038389
armentanoc <armentanocarolina@gmail.com>
2023-12-17 18:01:05 -0300

    Criação dos repositórios estáticos de funcionários, cardápio, mesa e pedidos.

---
+++ RestaurantManagement.Core/Servico/FuncionarioRepositorio.cs
@@ -0,0 +1,27 @@
+﻿
+using RestaurantManagement.Core.Modelos.Pessoas;
+
+namespace RestaurantManagement.Core.Servico
+{
+    internal class FuncionarioRepositorio
+    {
+        public static List<Funcionario> Funcionarios()
+        {
+            return new List<Funcionario>
+        {
+            new Garcom("Carol", "carol", 7000),
+            new Garcom("Paula", "paula", 7000),
+            new Gerente("Vitória", "vitoria", 12000)
+        };
+        } 
+        public static void ExibirFuncionarios()
+        {
+            Console.WriteLine("Funcionários:");
+
+            foreach (var funcionario in Funcionarios())
+            {
+                Console.WriteLine("\n" + funcionario.ToString());
+            }
+        }
+    }
+}

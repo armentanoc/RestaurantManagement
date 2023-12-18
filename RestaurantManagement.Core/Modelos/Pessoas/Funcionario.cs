a18c3184408bfcde04fc43d8987819b94f10e39f
armentanoc <armentanocarolina@gmail.com>
2023-12-18 10:58:12 -0300

    Ajustes nas classes de funcionários para abarcar os novos métodos.

--- RestaurantManagement.Core/Modelos/Pessoas/Funcionario.cs
+++ RestaurantManagement.Core/Modelos/Pessoas/Funcionario.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Linq;
+using System.Globalization;
 using RestaurantManagement.ConsoleInteraction;
 using RestaurantManagement.Core.Modelos.ItensCardapio;
 using RestaurantManagement.Core.Modelos.Mesas;
@@ -9,9 +9,9 @@ namespace RestaurantManagement.Core.Modelos.Pessoas
 {
     internal abstract class Funcionario : IFuncionario
     {
-        public string Nome { get; set; }
-        public string Login { get; set; }
-        public decimal Salario { get; set; }
+        public string Nome { get; private set; }
+        public string Login { get; private set; }
+        public decimal Salario { get; private set; }
 
         public Funcionario(string nome, string login, decimal salario)
         {
@@ -25,13 +25,23 @@ namespace RestaurantManagement.Core.Modelos.Pessoas
         public virtual void AlocarClientesEmMesa()
         {
             var mesasDisponiveis = MesaRepositorio.MesasDisponiveis().ToArray();
-            var menuMesasDisponiveis = mesasDisponiveis.Select(mesa => $"Mesa nº.: {mesa.Numero}").ToArray();
+            var menuMesasDisponiveis = mesasDisponiveis.Select(mesa => $"Mesa nº.: {mesa.Numero}").Concat(new[] { "Voltar" }).ToArray();
 
             if (mesasDisponiveis.Any())
             {
                 Menu opcoes = new Menu(menuMesasDisponiveis);
                 int mesaSelecionada = opcoes.ExibirMenu(Titulo.Principal());
-                MesaRepositorio.AlocarClientesNaMesa(mesasDisponiveis[mesaSelecionada]);
+
+                if (mesaSelecionada == mesasDisponiveis.Length) // "Voltar" option
+                {
+                    Console.WriteLine("Retornando ao menu anterior...");
+                }
+                else
+                {
+                    Mesa mesa = mesasDisponiveis[mesaSelecionada];
+                    MesaRepositorio.AlocarClientesNaMesa(mesa);
+                    Console.WriteLine($"Clientes alocados na mesa {mesa.Numero}");
+                }
             }
             else
             {
@@ -49,7 +59,7 @@ namespace RestaurantManagement.Core.Modelos.Pessoas
                 Menu opcoes = new Menu(menuMesasComClientes);
                 int mesaSelecionada = opcoes.ExibirMenu(Titulo.Principal());
 
-                Pedido pedido = mesasComClientes[mesaSelecionada].PedidoAtual;
+                Pedido? pedido = mesasComClientes[mesaSelecionada].PedidoAtual;
 
                 if (pedido == null)
                 {
@@ -61,7 +71,6 @@ namespace RestaurantManagement.Core.Modelos.Pessoas
 
                 TentarAdicionarPedido(pedido);
 
-                PedidoRepositorio.ExibirPedidos();
             }
             else
             {
@@ -114,12 +123,13 @@ namespace RestaurantManagement.Core.Modelos.Pessoas
         {
             Console.WriteLine("Escolha o item:");
 
-            for (int i = 0; i < itensDisponiveis.Count; i++)
-            {
-                Console.WriteLine($"{i}. {itensDisponiveis[i].Nome} - {itensDisponiveis[i].Preco:C}");
-            }
+            string[] menuItens = itensDisponiveis
+                .Select((produto, index) => $"{produto.Nome} - {produto.Preco:C}")
+                .Concat(new[] { "Voltar" }) // Add "Voltar" at the end
+                .ToArray();
 
-            int escolhaItem = Menu.LerInteiro("Digite o número do item desejado:");
+            Menu opcoesItens = new Menu(menuItens);
+            int escolhaItem = opcoesItens.ExibirMenu("\nDigite o número do item desejado:");
 
             if (escolhaItem >= 0 && escolhaItem < itensDisponiveis.Count)
             {
@@ -128,12 +138,16 @@ namespace RestaurantManagement.Core.Modelos.Pessoas
                 if (quantidade > 0)
                 {
                     pedido.AdicionarItens(itensDisponiveis[escolhaItem], quantidade);
-                    Console.WriteLine($"Item '{itensDisponiveis[escolhaItem].Nome}' adicionado ao pedido.");
+                    Console.WriteLine($"\nItem '{itensDisponiveis[escolhaItem].Nome}' adicionado ao pedido da mesa {pedido.GetMesa().Numero}.");
                 }
                 else
                 {
-                    Console.WriteLine("Quantidade inválida. Item não adicionado ao pedido.");
+                    Console.WriteLine("\nQuantidade inválida. Item não adicionado ao pedido.");
                 }
+            }
+            else if (escolhaItem == itensDisponiveis.Count) // "Voltar" 
+            {
+                Console.WriteLine("Retornando ao menu anterior...");
             }
             else
             {
@@ -143,7 +157,7 @@ namespace RestaurantManagement.Core.Modelos.Pessoas
 
         public override string ToString()
         {
-            return $"{Nome} - {Salario} - {GetType().ToString().Remove(0, 42)}";
+            return $"{Nome} - {Salario.ToString("C", CultureInfo.GetCultureInfo("pt-BR"))} - {GetType().ToString().Remove(0, 42)}";
         }
     }
 }
--- RestaurantManagement.Core/Modelos/Pessoas/Gerente.cs
+++ RestaurantManagement.Core/Modelos/Pessoas/Gerente.cs
@@ -1,5 +1,8 @@
 ﻿
 using RestaurantManagement.ConsoleInteraction;
+using RestaurantManagement.Core.Modelos.Enum;
+using RestaurantManagement.Core.Servico;
+using System.Linq;
 
 namespace RestaurantManagement.Core.Modelos.Pessoas
 {
@@ -10,6 +13,104 @@ namespace RestaurantManagement.Core.Modelos.Pessoas
         {
             Console.Clear();
             Console.WriteLine(Titulo.OlaGerente());
+            MenuOpcoesGerente();
+        }
+
+        private void MenuOpcoesGerente()
+        {
+            string[] menuGerente = { "Fechar Conta", "Processar Pagamento", "Sair" };
+            Menu opcoes = new Menu(menuGerente);
+
+            while (true)
+            {
+                Console.Clear();
+                int selecaoGerente = opcoes.ExibirMenu(Titulo.OlaGerente());
+                if (!AnalisarMenuOpcoesGerente(selecaoGerente))
+                {
+                    break;
+                }
+            }
+        }
+        private bool AnalisarMenuOpcoesGerente(int selecaoGerente)
+        {
+            Pedido pedido;
+            switch (selecaoGerente)
+            {
+                case 0:
+                    pedido = EscolherPedidoParaFecharConta();
+                    if(pedido != null) {
+                        pedido.FecharConta();
+                    }
+                    Menu.AguardarEntrada();
+                    return true;
+                case 1:
+                    pedido = EscolherPedidoParaProcessarPagamento();
+                    if (pedido != null)
+                    {
+                        pedido.ProcessarPagamento();
+                    }
+                    Menu.AguardarEntrada();
+                    return true;
+                case 2:
+                    Console.WriteLine("Escolheu sair.");
+                    return false;
+                default:
+                    return true;
+            }
+        }
+        private Pedido EscolherPedidoParaProcessarPagamento()
+        {
+            var pedidos = PedidoRepositorio.Pedidos().Where(pedido => pedido._Pagamento != null && pedido._Pagamento.FoiPago == false).ToList();
+            return AnalisarPedido(pedidos);
+        }
+        private Pedido EscolherPedidoParaFecharConta()
+        {
+            var pedidos = PedidoRepositorio.Pedidos().Where(pedido => pedido._Pagamento == null).ToList();
+            return AnalisarPedido(pedidos);
+        }
+        private Pedido AnalisarPedido(List<Pedido> pedidos)
+        {
+            if (pedidos.Any())
+            {
+                int totalPedidos = pedidos.Count;
+                string[] menuMesas = new string[totalPedidos + 1];
+                ExibirMesas(menuMesas, totalPedidos, pedidos);
+                return ExibirMenuMesas(menuMesas, totalPedidos, pedidos);
+            }
+            else
+            {
+                Console.WriteLine("Não há mesas nessa situação...");
+                return null;
+            }
+        }
+        private Pedido ExibirMenuMesas(string[] menuMesas, int totalPedidos, List<Pedido> pedidos)
+        {
+            Menu opcoes = new Menu(menuMesas);
+            menuMesas[totalPedidos] = "Voltar";
+
+            while (true)
+            {
+                Console.Clear();
+                int selecaoGerente = opcoes.ExibirMenu(Titulo.OlaGerente());
+
+                if (selecaoGerente == totalPedidos)
+                {
+                    Console.WriteLine("Retornando ao menu anterior...");
+                    return null;
+                }
+                else
+                {
+                    return pedidos[selecaoGerente];
+                }
+            }
+        }
+        private void ExibirMesas(string[] menuMesas, int totalPedidos, List<Pedido> pedidos)
+        {
+            Console.WriteLine("Pedidos disponíveis:");
+            for (int i = 0; i < totalPedidos; i++)
+            {
+                menuMesas[i] = $"Pedido da Mesa {pedidos[i].GetMesa().Numero}";
+            }
         }
     }
 }

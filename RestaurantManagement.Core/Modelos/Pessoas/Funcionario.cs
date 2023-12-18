86535a3ba3d671d91d8ca2ee822ea0a3ce4e8608
Igor Nunes <inunes.us@gmail.com>
2023-12-18 12:42:25 -0300

    corrigir posicao de adicionar pedido

--- RestaurantManagement.Core/Modelos/Pessoas/Funcionario.cs
+++ RestaurantManagement.Core/Modelos/Pessoas/Funcionario.cs
@@ -71,10 +71,8 @@ namespace RestaurantManagement.Core.Modelos.Pessoas
                     {
                         pedido = new Pedido(mesasComClientes[mesaSelecionada]);
                         mesasComClientes[mesaSelecionada].AtualizarPedidoAtual(pedido);
-                        
+                        PedidoRepositorio.AdicionarPedido(pedido);   
                     }
-
-                    PedidoRepositorio.AdicionarPedido(pedido);
 
                     while (EscolherPratoOuBebida(pedido))
                     {
